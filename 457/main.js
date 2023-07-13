4df7215073b6e25fd06f430779fb77cb92cf84f3
Tomi Halko <tomi.halko@gmail.com>
2023-07-13 17:10:10 +0300

    subsurface scattering for snowglobe

--- 457/main.js
+++ 457/main.js
@@ -22,7 +22,7 @@ const scoreText = document.getElementById('score-text');
 export const eventContainer = document.getElementById('event-container');
 export const timerText = document.getElementById('timer');
 
-let snowGlobe = new Spritesheet({x: clickCanvas.width/2, y: clickCanvas.height/2}, "textures/spriteSheet2k.png", 8, 8);
+let snowGlobe = new Spritesheet({x: clickCanvas.width/2, y: clickCanvas.height/2}, "textures/spriteSheet_default.png", 8, 8);
 snowGlobe.spritesheet.onload = function()
 {
     snowGlobe.PostInitialize();
