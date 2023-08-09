8371a5685aa25807a4ee85380fb63e239f769e27
Tomi Halko <tomi.halko@gmail.com>
2023-08-09 08:17:49 +0300

    test

---
+++ 663/main.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart(ctx, {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "World Wide Wine Production 2018"
+    }
+  }
+});
