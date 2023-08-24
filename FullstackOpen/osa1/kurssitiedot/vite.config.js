981688e02751e05d0adecd149b06fe552d541dda
Tomi Halko <tomi.halko@gmail.com>
2023-08-24 12:07:10 +0300

    React testi

--- FullstackOpen/osa1/kurssitiedot/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'https://tojutaha.github.io/FullstackOpen/osa1/kurssitiedot',
-  plugins: [react()],
-})
